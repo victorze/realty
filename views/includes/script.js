6f5868719ecb4a61a094b542854af51c4ede192f
Victor <victorze@protonmail.com>
2023-03-10 18:28:22 -0500

    Add script to prevent double submits

---
+++ views/includes/script.js
@@ -0,0 +1,9 @@
+// Prevent Double Submits
+document.querySelectorAll('form').forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    if (form.classList.contains('is-submitting')) {
+      e.preventDefault()
+    }
+    form.classList.add('is-submitting')
+  })
+})
