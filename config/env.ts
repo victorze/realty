3a7c1d237968087ccb8ddd10219659b47fec5b5c
Victor <victorze@protonmail.com>
2023-03-10 10:57:25 -0500

    Configure nodemailer

--- config/env.ts
+++ config/env.ts
@@ -13,3 +13,10 @@ export const DB_PORT = process.env.DB_PORT
 export const DB_DATABASE = process.env.DB_DATABASE
 export const DB_USERNAME = process.env.DB_USERNAME
 export const DB_PASSWORD = process.env.DB_PASSWORD
+
+export const MAIL_MAILER = process.env.MAIL_MAILER
+export const MAIL_HOST = process.env.MAIL_HOST
+export const MAIL_PORT = process.env.MAIL_PORT
+export const MAIL_USERNAME = process.env.MAIL_USERNAME
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD
+export const MAIL_FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS
---
+++ config/mail.ts
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer'
+import { env } from './index'
+
+export const transporter = nodemailer.createTransport({
+  host: env.MAIL_HOST,
+  port: Number(env.MAIL_PORT),
+  secure: false,
+  auth: {
+    user: env.MAIL_USERNAME,
+    pass: env.MAIL_PASSWORD,
+  },
+})
