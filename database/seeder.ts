214b0cc2747f8d67f0f61850484ff92758949c61
Victor <victorze@protonmail.com>
2023-03-14 19:14:47 -0500

    Create session table and refactor

---
+++ database/seeder.ts
@@ -0,0 +1,42 @@
+import { Category, Price } from '../models'
+import { dbConfig } from '../config'
+
+const categories = ['Casa', 'Departamento', 'Bodega', 'Terreno', 'Cabaña']
+const prices = [
+  '0 - $10,000 USD',
+  '$10,000 - $30,000 USD',
+  '$30,000 - $50,000 USD',
+  '$50,000 - $75,000 USD',
+  '$75,000 - $100,000 USD',
+  '$100,000 - $150,000 USD',
+  '$150,000 - $200,000 USD',
+  '$200,000 - $300,000 USD',
+  '$300,000 - $500,000 USD',
+  '+ $500,000 USD',
+]
+
+dbConfig.dataSource
+  .initialize()
+  .then(async () => {
+    // seed categories
+    await Category.delete({})
+    for (const category of categories) {
+      const newCategory = new Category()
+      newCategory.name = category
+      await newCategory.save()
+    }
+
+    //seed prices
+    await Price.delete({})
+    for (const price of prices) {
+      const newPrice = new Price()
+      newPrice.range = price
+      await newPrice.save()
+    }
+
+    process.exit()
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
---
+++ database/session.ts
@@ -0,0 +1,24 @@
+import { dbConfig } from '../config'
+
+dbConfig.dataSource
+  .initialize()
+  .then(async () => {
+    await dbConfig.dataSource.query(`
+      CREATE TABLE "session" (
+        "sid" varchar NOT NULL COLLATE "default",
+        "sess" json NOT NULL,
+        "expire" timestamp(6) NOT NULL
+      )
+      WITH (OIDS=FALSE);
+
+      ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+      CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+    `)
+
+    process.exit()
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
